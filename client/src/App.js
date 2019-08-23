d504c3d705f3f7a1bfce390a19805f0d1f2c3fd6
Alex <agordon1123@gmail.com>
2019-08-23 12:15:08 -0400

    established private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import PrivateRoute from './components/PrivateRoute';
 import LoginFormik from "./components/Login";
-import "./styles.scss";
 import BubblePage from "./components/BubblePage";
+import "./styles.scss";
 
 function App() {
   const [colorList, setColorList] = useState([]);
@@ -15,7 +15,7 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-        <Route path='/profile' component={BubblePage} />
+        <PrivateRoute path='/profile' component={BubblePage} />
       </div>
     </Router>
   );
